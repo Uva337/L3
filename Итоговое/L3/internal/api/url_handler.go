c9950987d57327cfc598629283ecb25ed8762a0e
Uva337 <66168613+Uva337@users.noreply.github.com>
2026-05-10 10:37:53 +0300

    Update url_handler.go

--- Итоговое/L3/internal/api/url_handler.go
+++ Итоговое/L3/internal/api/url_handler.go
@@ -16,13 +16,12 @@ func NewURLHandler(s *service.URLShortenerService) *URLHandler {
 	return &URLHandler{service: s}
 }
 
-// Структура того, что мы ждем от пользователя (POST /shorten)
 type shortenRequest struct {
 	OriginalURL string `json:"original_url" binding:"required"`
-	CustomAlias string `json:"custom_alias"` // Опционально (пользовательское имя)
+	CustomAlias string `json:"custom_alias"`
 }
 
-// Shorten — создание короткой ссылки
+
 func (h *URLHandler) Shorten(c *ginext.Context) {
 	var req shortenRequest
 
@@ -33,12 +32,10 @@ func (h *URLHandler) Shorten(c *ginext.Context) {
 
 	url, err := h.service.ShortenURL(c.Request.Context(), req.OriginalURL, req.CustomAlias)
 	if err != nil {
-		// Если кастомный алиас уже занят кем-то другим, БД вернет ошибку
 		c.JSON(http.StatusInternalServerError, ginext.H{"error": "Ошибка сохранения (возможно, имя уже занято): " + err.Error()})
 		return
 	}
 
-	// Возвращаем результат. Для удобства сразу склеиваем полную готовую ссылку
 	c.JSON(http.StatusCreated, ginext.H{
 		"id":           url.ID,
 		"short_code":   url.ShortCode,
@@ -47,11 +44,9 @@ func (h *URLHandler) Shorten(c *ginext.Context) {
 	})
 }
 
-// Redirect — магия перенаправления (GET /s/{code})
 func (h *URLHandler) Redirect(c *ginext.Context) {
 	shortCode := c.Param("code")
 
-	// Вытаскиваем информацию об устройстве пользователя из HTTP-заголовков
 	userAgent := c.Request.UserAgent()
 
 	originalURL, err := h.service.ProcessRedirect(c.Request.Context(), shortCode, userAgent)
@@ -60,11 +55,9 @@ func (h *URLHandler) Redirect(c *ginext.Context) {
 		return
 	}
 
-	// Выполняем 302 редирект на оригинальный сайт!
 	c.Redirect(http.StatusFound, originalURL)
 }
 
-// Analytics — получение статистики (GET /analytics/{code})
 func (h *URLHandler) Analytics(c *ginext.Context) {
 	shortCode := c.Param("code")
 
