347cefa5d4ffb8b3caa3e4010e52e59f43064e65
Uva337 <66168613+Uva337@users.noreply.github.com>
2026-05-10 10:49:59 +0300

    Update redis.go

--- Итоговое/L3/internal/storage/redis.go
+++ Итоговое/L3/internal/storage/redis.go
@@ -5,17 +5,17 @@ import (
 	"encoding/json"
 	"time"
 
-	"notification-service/internal/model" // Убедись, что путь к твоей модели правильный
+	"notification-service/internal/model" 
 
 	"github.com/redis/go-redis/v9"
 )
 
-// RedisStorage — обертка над клиентом Redis
+// обертка над клиентом Redis
 type RedisStorage struct {
 	client *redis.Client
 }
 
-// NewRedisStorage — конструктор подключения
+//  конструктор подключения
 func NewRedisStorage(addr, password string, db int) *RedisStorage {
 	client := redis.NewClient(&redis.Options{
 		Addr:     addr,
@@ -25,9 +25,9 @@ func NewRedisStorage(addr, password string, db int) *RedisStorage {
 	return &RedisStorage{client: client}
 }
 
-// SetNotification — сохраняем уведомление в кэш на 1 час
+//охраняем уведомление в кэш на 1 час
 func (r *RedisStorage) SetNotification(ctx context.Context, notif *model.Notification) error {
-	// Redis хранит строки или байты, поэтому превращаем структуру в JSON
+
 	data, err := json.Marshal(notif)
 	if err != nil {
 		return err
@@ -37,25 +37,23 @@ func (r *RedisStorage) SetNotification(ctx context.Context, notif *model.Notific
 	return r.client.Set(ctx, "notify:"+notif.ID, data, time.Hour).Err()
 }
 
-// GetNotification — пытаемся найти уведомление в кэше
+//  пытаемся найти уведомление в кэше
 func (r *RedisStorage) GetNotification(ctx context.Context, id string) (*model.Notification, error) {
 	val, err := r.client.Get(ctx, "notify:"+id).Result()
 	if err != nil {
-		return nil, err // Если кэша нет, вернет ошибку redis.Nil
+		return nil, err
 	}
 
-	// Превращаем JSON обратно в структуру Go
 	var notif model.Notification
 	err = json.Unmarshal([]byte(val), &notif)
 	return &notif, err
 }
 
 func (r *RedisStorage) SetURL(ctx context.Context, shortCode, originalURL string) error {
-	// Кэшируем ссылку на время
 	return r.client.Set(ctx, "url:"+shortCode, originalURL, 24*time.Hour).Err()
 }
 
-// GetURL — получаем длинный URL из кэша
+//  получаем длинный URL из кэша
 func (r *RedisStorage) GetURL(ctx context.Context, shortCode string) (string, error) {
 	return r.client.Get(ctx, "url:"+shortCode).Result()
 }
